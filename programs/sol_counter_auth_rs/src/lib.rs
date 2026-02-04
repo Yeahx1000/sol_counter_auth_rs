686585b26f1143c72b3a275ab496c9942b05bff3
Ekene <83151271+Yeahx1000@users.noreply.github.com>
2026-02-04 10:25:52 -0500

    implement `lib.rs` logic

--- programs/sol_counter_auth_rs/src/lib.rs
+++ programs/sol_counter_auth_rs/src/lib.rs
@@ -3,15 +3,88 @@ use anchor_lang::prelude::*;
 
 declare_id!("TO_REPLACE");
 
-// #[program]
-// pub mod sol_counter_auth_rs {
-//     use super::*;
+// won't compile without the actual program id.
+//TODO: Add actual program id.
 
-//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-//         msg!("Greetings from: {:?}", ctx.program_id);
-//         Ok(())
-//     }
-// }
+#[program]
+pub mod sol_counter_auth_rs {
+    use super::*;
 
-// #[derive(Accounts)]
-// pub struct Initialize {}
+    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = authority;
+        counter.count = 0;
+
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        require_keys_eq!(
+            ctx.accounts.authority.key(),
+            counter.authority,
+            CounterError::Unauthorized
+        );
+
+        counter.count = counter.count.checked_add(1).ok_or(CounterError::Overflow)?;
+
+        Ok(())
+    }
+
+    pub fn set_authority(ctx: Context<SetAuthority>, new_authority: Pubkey) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        require_keys_eq!(
+            ctx.accounts.authority.key(),
+            counter.authority,
+            CounterError::Unauthorized
+        );
+
+        counter.authority = new_authority;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = authority, space = 8 + Counter::INIT_SPACE)]
+    pub counter: Account<'info, Counter>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+
+    pub authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SetAuthority<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+
+    pub authority: Signer<'info>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
+}
+
+#[error_code]
+pub enum CounterError {
+    #[msg("Unauthorized Access: only the those with proper permissions can increment the counter")]
+    Unauthorized,
+    #[msg("Counter has overflowed")]
+    Overflow,
+}
